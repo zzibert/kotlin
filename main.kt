d79f9f6f0b1f690e21e6f77c197a189f27492d74
zan <zan_zibert@hotmail.com>
2019-01-05 19:46:48 +0100

    simple function

--- main.kt
+++ main.kt
@@ -1,2 +1,10 @@
 fun main(args: Array<String>)
-    { println("Hello, world!") }
+    {
+        println(max(2,3))
+    }
+
+fun max(a: Int, b: Int): Int
+    {
+        return if (a > b) a
+        else b
+    }
