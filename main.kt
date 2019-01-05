0c563f9b648be4bd919b308a4992b2892f640997
zan <zan_zibert@hotmail.com>
2019-01-05 20:07:44 +0100

    expression body

--- main.kt
+++ main.kt
@@ -3,8 +3,4 @@ fun main(args: Array<String>)
         println(max(2,3))
     }
 
-fun max(a: Int, b: Int): Int
-    {
-        return if (a > b) a
-        else b
-    }
+fun max(a: Int, b: Int): Int = if (a > b) a else b
