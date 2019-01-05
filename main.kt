0cb867ac67200e039c9308dc76ad9151b441d966
zan <zan_zibert@hotmail.com>
2019-01-05 20:13:32 +0100

    variables

--- main.kt
+++ main.kt
@@ -1,6 +1,10 @@
 fun main(args: Array<String>)
     {
+        val question = "the ultimate question!"
+        
+        val answer = 42
+        
         println(max(2,3))
     }
 
-fun max(a: Int, b: Int): Int = if (a > b) a else b
+fun max(a: Int, b: Int) = if (a > b) a else b
